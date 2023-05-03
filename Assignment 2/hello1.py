d14ab54db34cd4fe75b54c24097d8f1973a4e4f4
comdori <02145s1@gmail.com>
2023-05-04 00:29:37 +0900

    Create hello1.py

---
+++ Assignment 2/hello1.py
@@ -0,0 +1,11 @@
+a = 5 #a변수는 5이다.
+if (a > 3): #a가 3보다 클때 아래의 문자열을 출력한다.
+    print("Hello World") 
+    print("Hello TEAMLAB")
+if (a > 4):
+    print("Hello World")
+    print("Hello TEAMLAB")
+if (a > 5):
+    print("Hello World")
+    print("Hello TEAMLAB")
+    #코드가 반복되서 사용된다. 이것을 줄이려면 def 함수를 사용하면 된다.
