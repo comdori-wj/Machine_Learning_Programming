4ac051c22e03c8e0a3098cbd828bf24f7e77e42f
comdori <02145s1@gmail.com>
2023-05-05 05:05:13 +0900

    Create hello2.py

---
+++ Assignment 2/hello2.py
@@ -0,0 +1,14 @@
+def print_hello(): #아래의 소스를 묶어준다.
+    print("Hello World")
+    print("Hello TEAMLAB")
+
+a = 5
+if (a > 3):
+    print_hello() #위의 선언한 변수를 적어주면 hello1과 같이 반복안하고도 사용 할 수 있다.
+
+if (a > 4):
+    print_hello()
+
+if (a > 5):
+    print_hello()
+
