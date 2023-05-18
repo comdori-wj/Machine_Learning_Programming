620aeef836b863dfc5306c7ea51692703e044068
comdori <02145s1@gmail.com>
2023-05-18 22:33:56 +0900

    Create rectanglr_area.py

---
+++ Assignment 2/rectanglr_area.py
@@ -0,0 +1,9 @@
+def calculate_rectangle_area(x,y): # 함수정의가 되므로 실행은 안됨.
+    return x*y
+rectangle_x = 10 # 2개의 변수에 값이 할당됨을 알림.
+rectangle_y = 20
+print("사각형 x의 길이:", rectangle_x) # 길이를 출력
+print("사격형 y의 길이:", rectangle_y)
+
+# 넓이를 구하는 함수 호출
+print("사각형의 넓이:",calculate_rectangle_area(rectangle_x, rectangle_y)) # 사각형의 길이 변수를 곱하여 결과를 출력.
